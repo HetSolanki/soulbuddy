35f3eac392fc510ec7a24a475550eebc683d8274
H.$ <het.solanki109@gmail.com>
2025-01-19 01:25:45 +0530

    slight updates

--- app/api/getscore/route.ts
+++ app/api/getscore/route.ts
@@ -5,5 +5,5 @@ export const POST = async (request: NextRequest) => {
   const data = await request.json();
   const analysis = getCompleteNumerologyAndHoroscope(data.birthdate, data.name);
 
-  return NextResponse.json({ data: analysis });
+  return NextResponse.json({ ...analysis });
 };
